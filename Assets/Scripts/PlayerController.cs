2639045f0f9c43c7ddd7526d3c9246def3548a45
Adam <adam.swaggoner@gmail.com>
2017-11-17 19:34:16 -0700

    Implement Basic Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    [SerializeField]
+    private float _Speed;
+
+    /// <summary>
+    /// Maps this controller to a specific set of controls
+    /// </summary>
+    [Range(1, 2)]
+    [SerializeField]
+    private int _PlayerNum = 1;
+
+    private string _InputAxis;
+
+    private Transform _PointerTransform;
+
 	// Use this for initialization
 	void Start () {
-		
+        _InputAxis = "Player" + _PlayerNum.ToString();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetAxis(_InputAxis) != 0)
+        {
+            transform.position += transform.up * _Speed * Time.deltaTime * Input.GetAxis(_InputAxis);
+        }
 	}
 }
